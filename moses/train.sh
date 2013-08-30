4faaa48cb535d99dc83e81b7c3ff7f12783453be
Jacqueline Lee <chiaying@csail.mit.edu>
2013-08-30 18:58:50 -0400

    changed to path to lm

--- moses/train.sh
+++ moses/train.sh
@@ -4,5 +4,5 @@ data_dir="$ROOT/swahili/data/"
 root_dir="$MOSESROOT/mosesdecoder/"
 work_dir=$1
 
-$root_dir/scripts/training/train-model.perl -root-dir $1 -corpus $data_dir/$2 -f swa -e eng -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:7:/usr/users/chiaying/mosesdecoder/srilm/lm/train.eng.swa.7gram.arpa  -external-bin-dir $root_dir/tools/  -first-step=$3 -last-step=$4 >& $1.out
+$root_dir/scripts/training/train-model.perl -root-dir $1 -corpus $data_dir/$2 -f swa -e eng -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:7:$ROOT/lm/train.eng.swa.7gram.arpa  -external-bin-dir $root_dir/tools/  -first-step=$3 -last-step=$4 >& $1.out
 
